2f37044a44685f408e10954734d86de0ea52ac71
s19787 <s19787@pjwstk.edu.pl>
2024-03-18 18:15:47 +0100

    Base structure pt. 2

---
+++ BaseClasses/FluidContainer.cs
@@ -0,0 +1,26 @@
+﻿using Cwiczenia2.Cargo;
+using Cwiczenia2.Interfaces;
+
+namespace Cwiczenia2.BaseClasses;
+
+public class FluidContainer : Container<Fluid>, IHazardNotifier
+{
+    public FluidContainer(int weight, int height, int containerWeight, int containerDepth, int maxWeight, string serialNumber) : base(weight, height, containerWeight, containerDepth, maxWeight, serialNumber)
+    {
+    }
+
+    public override void UnloadWeight()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void LoadWeight(Fluid cargo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyHazadr(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Interfaces/IHazardNotifier.cs
@@ -0,0 +1,6 @@
+﻿namespace Cwiczenia2.Interfaces;
+
+public interface IHazardNotifier
+{
+    public void NotifyHazadr(string message);
+}
