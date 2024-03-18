4723ec9ac2ff5eee5f59a7bd6d90d1f98efd33f0
s19787 <s19787@pjwstk.edu.pl>
2024-03-18 18:36:30 +0100

    Introduced enum for (non)hazardous material types.

--- BaseClasses/FluidContainer.cs
+++ BaseClasses/FluidContainer.cs
@@ -1,13 +1,21 @@
 ﻿using Cwiczenia2.Cargo;
+using Cwiczenia2.Enums;
 using Cwiczenia2.Interfaces;
 
 namespace Cwiczenia2.BaseClasses;
 
 public class FluidContainer : Container<Fluid>, IHazardNotifier
 {
-    public FluidContainer(int weight, int height, int containerWeight, int containerDepth, int maxWeight, string serialNumber) : base(weight, height, containerWeight, containerDepth, maxWeight, serialNumber)
+    public ProductType Type { get; set; } //TODO enum ProductType
+    public FluidContainer(int weight, int height, int containerWeight, int containerDepth, int maxWeight, string serialNumber, ProductType productType) : base(weight, height, containerWeight, containerDepth, maxWeight, serialNumber)
     {
+        Type = productType;
     }
+
+    /*public static FluidContainer create(int weight, int height, int containerWeight, int containerDepth, int maxWeight, string serialNumber) : base(weight, height, containerWeight, containerDepth, maxWeight, serialNumber)
+    {
+        return new FluidContainer(Weight, Height, ContainerWeight, ContainerDepth, MaxWeight, "C",    );
+    }*/
 
     public override void UnloadWeight()
     {
@@ -16,6 +24,7 @@ public class FluidContainer : Container<Fluid>, IHazardNotifier
 
     public override void LoadWeight(Fluid cargo)
     {
+        
         //TODO jeśli ilość cargo jest większa niż maxPoj kontenera - OverfillException, itp.
         Weight = cargo.Amount;
     }
---
+++ Enums/ProductType.cs
@@ -0,0 +1,7 @@
+﻿namespace Cwiczenia2.Enums;
+
+public enum ProductType
+{
+    Normal = 0,
+    Hazardous = 1
+}
